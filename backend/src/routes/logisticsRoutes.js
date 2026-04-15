88dafde5894c19136dba6ead0b3ed43d1d443f2d
VanHoang-317 <hoangkirot@gmail.com>
2026-04-15 22:04:19 +0700

    Delete backend/src/routes/logisticsRoutes.js

--- backend/src/routes/logisticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createShipment } = require('../controllers/logisticsController');
-const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.post('/ship', authMiddleware, adminMiddleware, createShipment);
-
-module.exports = router;
